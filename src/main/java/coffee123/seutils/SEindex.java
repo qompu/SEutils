598ea347b98183c56d8e32c96f87326d1f0da645
qompu <mtamayo11@hawkmail.hccfl.edu>
2021-03-20 10:48:33 -0400

    new commit

---
+++ src/main/java/coffee123/seutils/SEindex.java
@@ -0,0 +1,165 @@
+/*
+ * Lucene index files im s directory. Updated for Lucene 8.8.1
+ * Original source: Lucene - Index and Search Text Files - HowToDoInJava.com
+ * https://howtodoinjava.com/lucene/lucene-index-and-search-text-files/
+ */
+package coffee123.seutils;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.FileVisitResult;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.SimpleFileVisitor;
+import java.nio.file.attribute.BasicFileAttributes;
+ 
+import org.apache.lucene.analysis.Analyzer;
+import org.apache.lucene.analysis.standard.StandardAnalyzer;
+import org.apache.lucene.document.Document;
+import org.apache.lucene.document.Field;
+import org.apache.lucene.document.Field.Store;
+import org.apache.lucene.document.LongPoint;
+import org.apache.lucene.document.StringField;
+import org.apache.lucene.document.TextField;
+import org.apache.lucene.index.DirectoryReader;
+import org.apache.lucene.index.IndexReader;
+import org.apache.lucene.index.IndexWriter;
+import org.apache.lucene.index.IndexWriterConfig;
+import org.apache.lucene.index.IndexWriterConfig.OpenMode;
+import org.apache.lucene.index.Term;
+import org.apache.lucene.queryparser.classic.QueryParser;
+import org.apache.lucene.search.IndexSearcher;
+import org.apache.lucene.search.Query;
+import org.apache.lucene.search.TopDocs;
+import org.apache.lucene.store.Directory;
+import org.apache.lucene.store.FSDirectory;
+ 
+public class SEindex {
+        //directory contains the lucene indexes
+    private static final String INDEX_DIR = "c:/temp/write2indexedFiles";
+    
+    public static void main(String[] args) throws IOException, Exception    {
+        
+        System.out.println( "Indexing files" ); 
+        //Input folder
+        String docsPath = "c:/temp/readFiles";
+         
+        //Output folder
+        String indexPath = "c:/temp/indexedFiles";
+ 
+        //Input Path Variable
+        final Path docDir = Paths.get(docsPath);
+ 
+        try
+        {
+            //org.apache.lucene.store.Directory instance
+            Directory dir = FSDirectory.open( Paths.get(indexPath) );
+             
+            //analyzer with the default stop words
+            Analyzer analyzer = new StandardAnalyzer();
+             
+            //IndexWriter Configuration
+            IndexWriterConfig iwc = new IndexWriterConfig(analyzer);
+            iwc.setOpenMode(OpenMode.CREATE_OR_APPEND);
+             
+            //IndexWriter writes new index files to the directory
+            IndexWriter writer = new IndexWriter(dir, iwc);
+             
+            //Its recursive method to iterate all files and directories
+            indexDocs(writer, docDir);
+ 
+            writer.close();
+        } 
+        catch (IOException e) 
+        {
+            e.printStackTrace();
+        }
+        
+        
+        
+                //Create lucene searcher. It search over a single IndexReader.
+        IndexSearcher searcher = createSearcher();
+       
+               //Search indexed contents using search term
+        TopDocs foundDocs = searchInContent("agreeable", searcher);
+         
+        
+        
+    }
+     
+    static void indexDocs(final IndexWriter writer, Path path) throws IOException 
+    {
+        //Directory?
+        if (Files.isDirectory(path)) 
+        {
+            //Iterate directory
+            Files.walkFileTree(path, new SimpleFileVisitor<Path>() 
+            {
+                @Override
+                public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException 
+                {
+                    try
+                    {
+                        //Index this file
+                        indexDoc(writer, file, attrs.lastModifiedTime().toMillis());
+                    } 
+                    catch (IOException ioe) 
+                    {
+                        ioe.printStackTrace();
+                    }
+                    return FileVisitResult.CONTINUE;
+                }
+            });
+        } 
+        else
+        {
+            //Index this file
+            indexDoc(writer, path, Files.getLastModifiedTime(path).toMillis());
+        }
+    }
+ 
+    static void indexDoc(IndexWriter writer, Path file, long lastModified) throws IOException 
+    {
+        try (InputStream stream = Files.newInputStream(file)) 
+        {
+            //Create lucene Document
+            Document doc = new Document();
+             
+            doc.add(new StringField("path", file.toString(), Field.Store.YES));
+            doc.add(new LongPoint("modified", lastModified));
+            doc.add(new TextField("contents", new String(Files.readAllBytes(file)), Store.YES));
+             
+            //Updates a document by first deleting the document(s) 
+            //containing <code>term</code> and then adding the new
+            //document.  The delete and then add are atomic as seen
+            //by a reader on the same index
+            writer.updateDocument(new Term("path", file.toString()), doc);
+        }
+    }
+    
+    
+    //--------------------------------
+        private static IndexSearcher createSearcher() throws IOException  {
+        Directory dir = FSDirectory.open(Paths.get(INDEX_DIR));
+         
+        //It is an interface for accessing a point-in-time view of a lucene index
+        IndexReader reader = DirectoryReader.open(dir);
+         
+        //Index searcher
+        IndexSearcher searcher = new IndexSearcher(reader);
+        return searcher;
+    }
+        
+     private static TopDocs searchInContent(String textToFind, IndexSearcher searcher) throws Exception
+    {
+        //Create search query
+        QueryParser qp = new QueryParser("contents", new StandardAnalyzer());
+        Query query = qp.parse(textToFind);
+         
+        //search the index
+        TopDocs hits = searcher.search(query, 10);
+        return hits;
+    }
+    
+}
---
+++ src/main/java/coffee123/seutils/SEsearch.java
@@ -0,0 +1,95 @@
+/*
+ * Lucene search file index. Updated for Lucene 8.8.1
+ * Original source: Lucene - Index and Search Text Files - HowToDoInJava.com
+ * https://howtodoinjava.com/lucene/lucene-index-and-search-text-files/
+ */
+package coffee123.seutils;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.FileVisitResult;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.SimpleFileVisitor;
+import java.nio.file.attribute.BasicFileAttributes;
+ 
+import org.apache.lucene.analysis.Analyzer;
+import org.apache.lucene.analysis.standard.StandardAnalyzer;
+import org.apache.lucene.document.Document;
+import org.apache.lucene.document.Field;
+import org.apache.lucene.document.Field.Store;
+import org.apache.lucene.document.LongPoint;
+import org.apache.lucene.document.StringField;
+import org.apache.lucene.document.TextField;
+import org.apache.lucene.index.DirectoryReader;
+import org.apache.lucene.index.IndexReader;
+import org.apache.lucene.index.IndexWriter;
+import org.apache.lucene.index.IndexWriterConfig;
+import org.apache.lucene.index.IndexWriterConfig.OpenMode;
+import org.apache.lucene.index.Term;
+import org.apache.lucene.queryparser.classic.QueryParser;
+import org.apache.lucene.search.IndexSearcher;
+import org.apache.lucene.search.Query;
+import org.apache.lucene.search.ScoreDoc;
+import org.apache.lucene.search.TopDocs;
+import org.apache.lucene.search.TopScoreDocCollector;
+import org.apache.lucene.store.Directory;
+import org.apache.lucene.store.FSDirectory;
+ 
+public class SEsearch {
+    //directory contains the lucene indexes
+    private static final String INDEX_DIR = "c:/temp/indexedFiles";
+    
+            //Input folder C:\\example\\dir
+    private static final String docsPath = "c:/temp/readFiles";
+         
+        //Output folder
+    private static final String indexPath = "c:/temp/write2indexedFiles";
+ 
+
+    public static void main(String[] args) throws Exception 
+    {
+        //Create lucene searcher. It search over a single IndexReader.
+        IndexSearcher searcher = createSearcher();
+         
+        //Search indexed contents using search term
+        //  TopDocs foundDocs = searchInContent("agreeable", searcher); // test
+        TopDocs foundDocs = searchInContent("voluptate", searcher);  // test
+           
+        //Total found documents
+        System.out.println("Total Results :: " + foundDocs.totalHits);
+         
+        //Let's print out the path of files which have searched term
+        for (ScoreDoc sd : foundDocs.scoreDocs) 
+        {
+            Document d = searcher.doc(sd.doc);
+            System.out.println("Path : "+ d.get("path") + ", Score : " + sd.score);
+        }
+    }
+     
+    private static TopDocs searchInContent(String textToFind, IndexSearcher searcher) throws Exception
+    {
+        //Create search query
+        QueryParser qp = new QueryParser("contents", new StandardAnalyzer());
+        Query query = qp.parse(textToFind);
+         
+        //search the index
+        TopDocs hits = searcher.search(query, 10);
+        return hits;
+    }
+ 
+    private static IndexSearcher createSearcher() throws IOException 
+    {
+        Directory dir = FSDirectory.open(Paths.get(INDEX_DIR));
+         
+        //It is an interface for accessing a point-in-time view of a lucene index
+        IndexReader reader = DirectoryReader.open(dir);
+         
+        //Index searcher
+        IndexSearcher searcher = new IndexSearcher(reader);
+        return searcher;
+    }
+}
+
+
