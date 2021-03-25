5cde9d7a85d5ff23ac563fe4d5974e1b2c03824a
Adriel Lopez <alopez216@hawkmail.hccfl.edu>
2021-03-25 19:22:08 -0400

    TestClass updated

--- src/main/java/coffee123/seutils/TestClass.java
+++ src/main/java/coffee123/seutils/TestClass.java
@@ -5,5 +5,25 @@ package coffee123.seutils;
 
 
 public class TestClass {
+    public static void main(String[] args){
+        
+        //calling class SEindex
+        SEindex index = new SEindex();
+        
+        //calling class SEsearch
+        SEsearch search = new SEsearch();
+        
+        
+        try{
+        
+        //statements to cause exceptions
+        
+        }catch(Exception e){
+        System.out.println("Exception occured");
+        
+        }
+        // error handleling code 
+        
+    }
     
 }
