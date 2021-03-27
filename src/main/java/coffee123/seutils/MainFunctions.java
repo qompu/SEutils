de31f8f9718827838c8d69e02b0bb982dc8b4da5
qompu <mtamayo11@hawkmail.hccfl.edu>
2021-03-27 15:20:06 -0400

    Added MainFunctions.java to hold and process the main Lucene functions which were moved from SEindex.java and SEsearch.java and changed their property to protected, so they can be accessed within the package by other classes.
    Added deleteEntriesFromIndexUsingTerm() to MainFunctions.java to remove items from the index containing a query term.
    It appears to be processing but does not commit the changes after testing the SEsearch.java. More debugging and testing is required.

---
+++ src/main/java/coffee123/seutils/MainFunctions.java
@@ -0,0 +1,191 @@
+/*
+ * Lucene main search and index functions. Updated for Lucene 8.8.1
+ * Adapted from original source code: 
+ * Lucene - Index and Search Text Files - HowToDoInJava.com
+ * https://howtodoinjava.com/lucene/lucene-index-and-search-text-files/
+ */
+
+
+package coffee123.seutils;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.FileVisitResult;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.SimpleFileVisitor;
+import java.nio.file.attribute.BasicFileAttributes;
+ 
+import org.apache.lucene.analysis.Analyzer;
+import org.apache.lucene.analysis.standard.StandardAnalyzer;
+import org.apache.lucene.document.Document;
+import org.apache.lucene.document.Field;
+import org.apache.lucene.document.Field.Store;
+import org.apache.lucene.document.LongPoint;
+import org.apache.lucene.document.StringField;
+import org.apache.lucene.document.TextField;
+import org.apache.lucene.index.DirectoryReader;
+import org.apache.lucene.index.IndexReader;
+import org.apache.lucene.index.IndexWriter;
+import org.apache.lucene.index.IndexWriterConfig;
+import org.apache.lucene.index.IndexWriterConfig.OpenMode;
+import org.apache.lucene.index.Term;
+import org.apache.lucene.queryparser.classic.QueryParser;
+import org.apache.lucene.search.IndexSearcher;
+import org.apache.lucene.search.Query;
+import org.apache.lucene.search.TopDocs;
+import org.apache.lucene.store.Directory;
+import org.apache.lucene.store.FSDirectory;
+
+//--
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.FileVisitResult;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.SimpleFileVisitor;
+import java.nio.file.attribute.BasicFileAttributes;
+ 
+import org.apache.lucene.analysis.Analyzer;
+import org.apache.lucene.analysis.standard.StandardAnalyzer;
+import org.apache.lucene.document.Document;
+import org.apache.lucene.document.Field;
+import org.apache.lucene.document.Field.Store;
+import org.apache.lucene.document.LongPoint;
+import org.apache.lucene.document.StringField;
+import org.apache.lucene.document.TextField;
+import org.apache.lucene.index.DirectoryReader;
+import org.apache.lucene.index.IndexReader;
+import org.apache.lucene.index.IndexWriter;
+import org.apache.lucene.index.IndexWriterConfig;
+import org.apache.lucene.index.IndexWriterConfig.OpenMode;
+import org.apache.lucene.index.Term;
+import org.apache.lucene.queryparser.classic.ParseException;
+import org.apache.lucene.queryparser.classic.QueryParser;
+import org.apache.lucene.search.IndexSearcher;
+import org.apache.lucene.search.Query;
+import org.apache.lucene.search.ScoreDoc;
+import org.apache.lucene.search.TermQuery;
+import org.apache.lucene.search.TopDocs;
+import org.apache.lucene.search.TopScoreDocCollector;
+import org.apache.lucene.store.Directory;
+import org.apache.lucene.store.FSDirectory;
+
+
+
+public class MainFunctions {
+    
+    private static final String INDEX_DIR = "c:/temp/indexedFiles";
+    
+    private static final String DOC_DIR = "c:/temp/readFiles";
+
+       protected static void indexDoc(IndexWriter writer, Path file, long lastModified) throws IOException 
+    {
+        try (InputStream stream = Files.newInputStream(file)) 
+        {
+            //Create lucene Document
+            Document doc = new Document();
+             
+            doc.add(new StringField("path", file.toString(), Field.Store.YES));
+            doc.add(new LongPoint("modified", lastModified));
+            doc.add(new TextField("contents", new String(Files.readAllBytes(file)), Store.YES));
+             
+            //Updates a document by first deleting the document(s) 
+            //containing <code>term</code> and then adding the new
+            //document.  The delete and then add are atomic as seen
+            //by a reader on the same index
+            writer.updateDocument(new Term("path", file.toString()), doc);
+        }
+    }
+       
+       
+protected static void indexDocs(final IndexWriter writer, Path path) throws IOException 
+    {
+        //Directory?
+        if (Files.isDirectory(path)) 
+        {
+            //Iterate directory
+            Files.walkFileTree(path, new SimpleFileVisitor<Path>() 
+            {
+                @Override
+                public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException 
+                {
+                    try
+                    {
+                        //Index this file
+                       indexDoc(writer, file, attrs.lastModifiedTime().toMillis());
+                    } 
+                    catch (IOException ioe) 
+                    {
+                        ioe.printStackTrace();
+                    }
+                    return FileVisitResult.CONTINUE;
+                }
+            });
+        } 
+        else
+        {
+            //Index this file
+           MainFunctions.indexDoc(writer, path, Files.getLastModifiedTime(path).toMillis());
+        }
+    }
+        
+        
+        
+  protected static IndexSearcher createSearcher() throws IOException  {
+         
+        Directory dir = FSDirectory.open(Paths.get(INDEX_DIR));
+         
+        //It is an interface for accessing a point-in-time view of a lucene index
+        IndexReader reader = DirectoryReader.open(dir);
+         
+        //Index searcher
+        IndexSearcher searcher = new IndexSearcher(reader);
+        return searcher;
+    }
+    
+protected static TopDocs searchInContent(String textToFind, IndexSearcher searcher) throws Exception
+    {
+        //Create search query
+        QueryParser qp = new QueryParser("contents", new StandardAnalyzer());
+        Query query = qp.parse(textToFind);
+         
+        //search the index
+        TopDocs hits = searcher.search(query, 10);
+        return hits;
+    }
+
+        // deleteEntriesFromIndexUsingTerm() not fully functional.
+        // Requires debugging and testing
+    	public static void deleteEntriesFromIndexUsingTerm() throws IOException, ParseException {
+
+	    //System.out.println("Deleting documents with field '" + term.field() + "' with text '" + term.text() + "'");
+	    System.out.println("Deleting index entries with defined term" );
+                
+            Directory directory = FSDirectory.open( Paths.get(INDEX_DIR) );
+             
+            //analyzer with the default stop words
+            Analyzer analyzer = new StandardAnalyzer();
+                
+            //IndexWriter Configuration
+            IndexWriterConfig iwc = new IndexWriterConfig(analyzer);
+            iwc.setOpenMode(OpenMode.CREATE_OR_APPEND);
+             
+            //IndexWriter writes new index files to the directory
+            IndexWriter indexWriter = new IndexWriter(directory, iwc);
+            
+            Term term = new Term("lorem", "ipsum");
+            Query query = new TermQuery(term);
+            
+            indexWriter.deleteDocuments(query);   // Requires debugging
+		
+            indexWriter.close();
+
+	}
+
+
+ 
+}
