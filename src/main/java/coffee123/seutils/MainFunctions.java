c0c7bf95db456b14c0310bbb4152e5d6ca3d7427
qompu <mtamayo11@hawkmail.hccfl.edu>
2021-03-30 17:24:59 -0400

    Added header comments

--- src/main/java/coffee123/seutils/MainFunctions.java
+++ src/main/java/coffee123/seutils/MainFunctions.java
@@ -3,42 +3,13 @@
  * Adapted from original source code: 
  * Lucene - Index and Search Text Files - HowToDoInJava.com
  * https://howtodoinjava.com/lucene/lucene-index-and-search-text-files/
+ * Additional debugging and troubleshooting of the deleteEntriesFromIndexUsingTerm() 
+ * method using the Luke utility (http://www.getopt.org/luke/) is required.
  */
 
 
 package coffee123.seutils;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.nio.file.FileVisitResult;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.nio.file.SimpleFileVisitor;
-import java.nio.file.attribute.BasicFileAttributes;
- 
-import org.apache.lucene.analysis.Analyzer;
-import org.apache.lucene.analysis.standard.StandardAnalyzer;
-import org.apache.lucene.document.Document;
-import org.apache.lucene.document.Field;
-import org.apache.lucene.document.Field.Store;
-import org.apache.lucene.document.LongPoint;
-import org.apache.lucene.document.StringField;
-import org.apache.lucene.document.TextField;
-import org.apache.lucene.index.DirectoryReader;
-import org.apache.lucene.index.IndexReader;
-import org.apache.lucene.index.IndexWriter;
-import org.apache.lucene.index.IndexWriterConfig;
-import org.apache.lucene.index.IndexWriterConfig.OpenMode;
-import org.apache.lucene.index.Term;
-import org.apache.lucene.queryparser.classic.QueryParser;
-import org.apache.lucene.search.IndexSearcher;
-import org.apache.lucene.search.Query;
-import org.apache.lucene.search.TopDocs;
-import org.apache.lucene.store.Directory;
-import org.apache.lucene.store.FSDirectory;
-
-//--
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -180,7 +151,7 @@ protected static TopDocs searchInContent(String textToFind, IndexSearcher search
             Term term = new Term("lorem", "ipsum");
             Query query = new TermQuery(term);
             
-            indexWriter.deleteDocuments(query);   // Requires debugging
+            indexWriter.deleteDocuments(query);   // Requires debugging using Luke
 		
             indexWriter.close();
 
