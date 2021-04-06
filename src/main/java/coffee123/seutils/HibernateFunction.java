d2a9629aafe4c0707497913e0b5727f7602c0a6f
Adriel Lopez <alopez216@hawkmail.hccfl.edu>
2021-04-06 19:51:29 -0400

    Hibernate configuration xml added

--- src/main/java/coffee123/seutils/HibernateFunction.java
+++ src/main/java/coffee123/seutils/HibernateFunction.java
@@ -4,11 +4,25 @@
  * and open the template in the editor.
  */
 package coffee123.seutils;
-
-/**
- *
- * @author Adriel
- */
+ 
+package com;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.AnnotationConfiguration;
 public class HibernateFunction {
-    
+   public static void delete(){
+   try {
+	SessionFactory sessionfactory=new AnnotationConfiguration().configure().buildSessionFactory();
+	Session session=sessionfactory.openSession();
+	Employee obj_Employee=new Employee();
+	obj_Employee.setEmployee_name("Employee Two");
+	session.delete(obj_Employee);
+	System.out.println("Deleted  "+obj_Employee.getEmployee_name());
+	session.beginTransaction().commit(); 
+        session.close();
+        sessionfactory.close();
+    } catch (Exception e) {
+	System.out.println(e);
+     }
+    }
 }
